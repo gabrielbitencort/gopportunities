68a625fefb1141e9dd22fcf239010675eb1f2e2b
Gabriel <gabrielbiten.henrique@gmail.com>
2023-12-16 15:37:27 -0300

    docs: add documentation for listOpenings

--- docs/docs.go
+++ docs/docs.go
@@ -200,6 +200,20 @@ const docTemplate = `{
                 }
             }
         },
+        "handler.ListOpeningsResponse": {
+            "type": "object",
+            "properties": {
+                "data": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/schemas.OpeningResponse"
+                    }
+                },
+                "message": {
+                    "type": "string"
+                }
+            }
+        },
         "handler.ShowOpeningResponse": {
             "type": "object",
             "properties": {
--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+// @BasePath /api/v1
+
+// @Summary List openings
+// @Description List all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [get]
 func ListOpeningHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
